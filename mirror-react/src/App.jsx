309e672c2b3975bb955b8f3ef99692216f457508
MathSoares88 <matheus.ssoares@sptech.school>
2025-04-13 17:11:06 -0300

    Adc react-router-dom (rotas para as páginas) e banco de dados para os cards dos hambirgueres

--- mirror-react/src/App.jsx
+++ mirror-react/src/App.jsx
@@ -1,23 +1,20 @@
+// src/App.jsx
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Favoritos from "./pages/Favoritos";
 import "./App.css";
 
-
-
-
 function App() {
   return (
-    <div>
-      {/* <Home /> */}
-      <Favoritos/>
-
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
-
-
